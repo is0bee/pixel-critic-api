6b7a333b5564d60be940321dfce9b1e31bb8c985
Victor Jerrysson <victorjerrysson@gmail.com>
2024-09-28 19:42:02 -0300

    refactor: removing unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import "../App.css";
-
-function App() {
+export default function App() {
 	return (
-		<div className="flex flex-col h-full w-auto items-center justify-center">
+		<div className="flex flex-col items-center justify-center w-auto h-full">
 			<h1 className="text-3xl font-bold">Bem vindo a pixel-critic-api</h1>
 			<h2 className="text-xl font-medium">Navegue por uma das nossas rotas de api</h2>
 
-			<ul className="mt-5 flex flex-col w-full items-start">
+			<ul className="flex flex-col items-start w-full mt-5">
 				<li>/auth/login</li>
 				<li>/auth/signup</li>
 				<li>/avatar/upload</li>
@@ -19,5 +17,3 @@ function App() {
 		</div>
 	);
 }
-
-export default App;
