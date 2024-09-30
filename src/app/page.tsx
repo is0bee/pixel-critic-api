e1d643c0bbc2e7212c66920f6f43d19f0c813e6a
Vitória Pio <pio.vitoria156@gmail.com>
2024-09-30 18:14:45 -0300

    feat: Adicionando shadcn e tailwind configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-export default function App() {
-	return (
-		<div className="flex flex-col items-center justify-center w-auto h-full">
-			<h1 className="text-3xl font-bold">Bem vindo a pixel-critic-api</h1>
-			<h2 className="text-xl font-medium">Navegue por uma das nossas rotas de api</h2>
+import { Button } from "@/components/ui/button";
 
-			<ul className="flex flex-col items-start w-full mt-6">
-				<li>/auth/login</li>
-				<li>/auth/signup</li>
-				<li>/avatar/upload</li>
-				<li>/followers</li>
-				<li>/profile/update</li>
-				<li>/reviews</li>
-				<li>/topfour</li>
-				<li>/user</li>
-			</ul>
-		</div>
-	);
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center w-auto h-full">
+      <h1 className="text-3xl font-bold">Bem vindo a pixel-critic-api</h1>
+      <h2 className="text-xl font-medium">
+        Navegue por uma das nossas rotas de api
+      </h2>
+
+      <Button>Primary</Button>
+      <Button variant={"outline"}>Outline</Button>
+    </div>
+  );
 }
